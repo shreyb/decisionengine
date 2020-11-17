dafcea714e1e00832e84d70db7a225c2189c72dc
Shreyas Bhat <sbhat@fnal.gov>
2020-11-17 12:14:03 -0600

    Change values() call to items() call since we don't use key

--- framework/taskmanager/TaskManager.py
+++ framework/taskmanager/TaskManager.py
@@ -471,7 +471,7 @@ class TaskManager:
         """
         if not data_block:
             return
-        for key, action_list in actions.items():
+        for action_list in actions.values():
             for action in action_list:
                 publisher = self.channel.publishers[action]
                 name = publisher.name
