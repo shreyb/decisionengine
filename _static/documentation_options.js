61678c72c08fa63933c046b0c2f5197ff8e4ca20
GitHub Action <action@github.com>
2021-08-03 17:47:10 +0000

    Update doc to match fed95c62237902f86cb54d4c03a5dc672e906689

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.0.post346+g33f9ade',
+    VERSION: '1.2.0.post363+gfed95c6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
